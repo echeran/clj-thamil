9d904bb2fed9556b48a035f3ebe4e68e8a703197
Elango Cheran <elango.cheran@gmail.com>
2014-09-05 19:54:41 -0700

    replaces map of quoted symbols with a quoted map of symbols for readability

--- src/clj_thamil/core.clj
+++ src/clj_thamil/core.clj
@@ -47,69 +47,69 @@
         `(translate-form-symbol '~old-form# '~new-form#))))
 
 
-(def fns-map {
-              'take 'எடு
-              'drop 'விடு
-              ;; 'inc 'ஏறுமானம்
-              'inc 'ஏற்று
-              ;; 'dec 'இறங்குமானம்
-              'dec 'இறக்கு
-              'range 'வீச்சு
-              'take-while 'எடு-என்னும்வரை
-              'drop-while 'விடு-என்னும்வரை
-              'interleave 'பின்னு
-              'reduce 'இறுக்கு
-              ;; 'reducer 'இறுக்குவர் ;; ??
-              'map 'விவரி
-              'hash-map 'புலவெண்-விவரணையாக்கம் 
-              ;; 'vector 'காவி ;; ??
-              ;; 'vector 'நெறியம் ;; ??
-              'list 'பட்டியல்
-              'set 'அமைவு
-              'hash-set 'புலவெண்-அமைவு
-              'atom 'அணு
-              'agent 'முகவர்
-              'first 'முதல்
-              'second 'இரண்டாம்
-              'last 'கடைசி
-              'butlast 'கடைசியின்றி
-              'rest 'மீதி
-              'next 'அடுத்த'
-              'true 'வாய்மை ;; should we use வாய்மை, மெய்மை, or உண்மை ?  i am
-              ;; thinking of using வாய்மை or மெய்மை so as to leave உண்மை to continue to
-              ;; be used in more casual / less formal situations
-              'false 'பொய்மை
-              'print 'அச்சிடு
-              'println 'வரி-அச்சிடு
-              'filter 'வடி
-              'remove 'அகற்று
-              'keep 'கொள்
-              'slurp 'உறிஞ்சு;; could be சப்பு
-              'spit 'ஊற்று ;; could be துப்பு
-              'seq 'வரிசை
-              'dorun 'செய்யோட்டம்
-              'doall 'செய்யெல்லாம்
-              'str 'சரம்
-              'interpose 'பிண்ணு
-              })
+(def fns-map '{
+               take எடு
+               drop விடு
+               ;; inc ஏறுமானம்
+               inc ஏற்று
+               ;; dec இறங்குமானம்
+               dec இறக்கு
+               range வீச்சு
+               take-while எடு-என்னும்வரை
+               drop-while விடு-என்னும்வரை
+               interleave பின்னு
+               reduce இறுக்கு
+               ;; reducer இறுக்குவர் ;; ??
+               map விவரி
+               hash-map புலவெண்-விவரணையாக்கம் 
+               ;; vector காவி ;; ??
+               ;; vector நெறியம் ;; ??
+               list பட்டியல்
+               set அமைவு
+               hash-set புலவெண்-அமைவு
+               atom அணு
+               agent முகவர்
+               first முதல்
+               second இரண்டாம்
+               last கடைசி
+               butlast கடைசியின்றி
+               rest மீதி
+               next அடுத்த
+               true வாய்மை ;; should we use வாய்மை, மெய்மை, or உண்மை ?  i am
+               ;; thinking of using வாய்மை or மெய்மை so as to leave உண்மை to continue to
+               ;; be used in more casual / less formal situations
+               false பொய்மை
+               print அச்சிடு
+               println வரி-அச்சிடு
+               filter வடி
+               remove அகற்று
+               keep கொள்
+               slurp உறிஞ்சு;; could be சப்பு
+               spit ஊற்று ;; could be துப்பு
+               seq வரிசை
+               dorun செய்யோட்டம்
+               doall செய்யெல்லாம்
+               str சரம்
+               interpose பிண்ணு
+               })
 
-(def forms-map {
-                'if 'எனில்
-                'when 'என்னும்போது
-                'if-not 'இல்லெனில்
-                'when-not 'இல்லென்னும்-போது
-                'def 'வரையறு
-                'fn 'செயல்கூறு
-                'defn 'வரையறு-செயல்கூறு
-                'let 'வைத்துக்கொள் ;; maybe just கொள்
-                'and 'மற்றும்
-                'or 'அல்லது
-                'not 'அன்று
-                ;; else என்றேல் ??  does அன்றி make sense?
-                'loop 'சுற்று
-                'doseq 'செய்வரிசை
-                ;; 'for 'ஒவ்வொன்றுக்கும்
-                'for 'ஒன்றொன்றுக்கு
+(def forms-map '{
+                 if எனில்
+                 when என்னும்போது
+                 if-not இல்லெனில்
+                 when-not இல்லென்னும்-போது
+                 def வரையறு
+                 fn செயல்கூறு
+                 defn வரையறு-செயல்கூறு
+                 let வைத்துக்கொள் ;; maybe just கொள்
+                 and மற்றும்
+                 or அல்லது
+                 not அன்று
+                 ;; else என்றேல் ??  does அன்றி make sense?
+                 loop சுற்று
+                 doseq செய்வரிசை
+                 ;; for ஒவ்வொன்றுக்கும்
+                 for ஒன்றொன்றுக்கு
                 })
 
 ;; do the actual "translation" for bindings, fns, and any other value
