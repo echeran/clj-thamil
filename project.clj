386033f47786c8e14922f711b28a09ff3cffca41
Elango Cheran <elango.cheran@gmail.com>
2014-10-13 23:29:25 -0700

    adds a letter frequency analysis namespace and makes it executable from an uberjar (shaded jar)

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/echeran/clj-thamil"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.csv "0.1.2"]]
   ;; this config seems to work with clojure 1.5.1 but may not 1.6.0
   ;; (if not, then I don't know why)
 
@@ -14,6 +15,9 @@
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
+  :aot [clj-thamil.format.analysis]
+
+  :main clj-thamil.format.analysis
 
   :cljx {:builds [{:source-paths ["src/cljx"]
                    :output-path "target/generated/src/clj"
