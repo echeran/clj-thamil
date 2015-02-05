d7bd654128848e356ee64ed5be5b8be94fe047d5
Elango Cheran <elango.cheran@gmail.com>
2015-02-04 22:05:25 -0800

    version 0.1.1 - Clojars deploy config

--- project.clj
+++ project.clj
@@ -1,13 +1,21 @@
-(defproject clj-thamil "0.1.0"
+(defproject clj-thamil "0.1.1"
   :description "A project encompassing various Thamil language-specific computing ideas"
   :url "https://github.com/echeran/clj-thamil"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/echeran/clj-thamil"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.csv "0.1.2"]]
   ;; this config seems to work with clojure 1.5.1 but may not 1.6.0
   ;; (if not, then I don't know why)
 
+  :repositories [["releases" {:url "https://clojars.org/repo/"
+                              ;; :sign-releases false
+                              }]]
+  :deploy-repositories [["clojars" {:creds :gpg}]]
+
+  
   :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
   :source-paths ["src" "target/generated/src/clj"]
   :resource-paths ["target/generated/src/cljs"]
@@ -48,8 +56,11 @@
   :aliases {"test-cljs" ["do" ["cljx" "once"] ["cljsbuild" "test"]]
             "test-all"  ["do" ["with-profile" "default:+1.6" "test"] ["cljsbuild" "test"]]}
 
+  :lein-release {:deploy-via :shell
+                 :shell ["lein" "deploy"]}
+
   :profiles {:dev {:plugins [[com.keminglabs/cljx "0.4.0"]]
                    :aliases {"cleantest" ["do" "clean," "cljx" "once," "test,"
                                           "cljsbuild" "test"]
-                             "deploy" ["do" "clean," "cljx" "once," "deploy" "clojars"]}}
+                             "deploy" ["do" "clean" ["cljx" "once"] ["deploy" "clojars"]]}}
              :provided {:dependencies [[org.clojure/clojurescript "0.0-2234"]]}})
