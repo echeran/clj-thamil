0166ac2905bd1c5e53fba0f237878585cec7fd05
Elango Cheran <elango.cheran@gmail.com>
2015-02-07 16:52:57 -0800

    update Clojure to 1.6 to enable simpler API for calls from Java

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/echeran/clj-thamil"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.csv "0.1.2"]]
   ;; this config seems to work with clojure 1.5.1 but may not 1.6.0
   ;; (if not, then I don't know why)
